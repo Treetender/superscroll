1132f8debe82ed25e347eda709ddfbd5642ca53d
Treetender <adam.acott@gmail.com>
2018-11-30 06:17:45 +0000

    Initial commit with faker data and animations!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatCardModule} from '@angular/material/card';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { InfiniteScrollComponent } from './infinite-scroll/infinite-scroll.component';
+import { BasicScrollComponent } from './basic-scroll/basic-scroll.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfiniteScrollComponent,
+    BasicScrollComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ScrollingModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
